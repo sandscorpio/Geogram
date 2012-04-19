c0ee48c654fb8fcc47aae3682b4aaa442ed4caa8
amit aggarwal <amit@mangobird.com>
2012-04-19 00:41:59 -0700

    all basic functionality

--- src/com/android/geogram/GeoInstagram.java
+++ src/com/android/geogram/GeoInstagram.java
@@ -2,6 +2,9 @@ package com.android.geogram;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
@@ -10,19 +13,47 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.os.AsyncTask;
 import android.util.Log;
 
 public class GeoInstagram {
 	private static final String TAG = "Geogram";
+	public static final String TOKEN = "INSTAGRAM-TOKEN";
+	private GeogramActivity _g;
 	
-	public void getPics() {
-		String uri = "https://api.instagram.com/v1/media/search?lat=48.858844&lng=2.294351&access_token=41015468.f59def8.737cbf60fcff4dd09058252899980109";
-		new RequestTask().execute(uri);
+	public GeoInstagram(GeogramActivity g) {
+		_g = g;
+	}
+
+	public void getPics(double latitude, double longitude, long timestart) {
+		String uri  = String.format("https://api.instagram.com/v1/media/search?lat=%f&lng=%f&min_timestamp=%d&access_token=%s&count=%d", latitude, longitude, timestart, TOKEN, 150);
+		//String uri  = String.format("https://api.instagram.com/v1/media/search?lat=%f&lng=%f&min_timestamp=%d&access_token=%s", latitude, longitude, timestart, TOKEN);
+		Log.d(TAG, uri);
+		new RequestTask(this).execute(uri);
+	}	
+
+	public void getPics(double latitude, double longitude) {
+		//String uri  = String.format("https://api.instagram.com/v1/media/search?lat=%f&lng=%f&min_timestamp=%d&access_token=%s&count=%d", latitude, longitude, timestart, TOKEN, 100);
+		String uri  = String.format("https://api.instagram.com/v1/media/search?lat=%f&lng=%f&access_token=%s", latitude, longitude, TOKEN);
+		Log.d(TAG, uri);
+		new RequestTask(this).execute(uri);
+	}	
+	
+	public void receivedPics(InstaData[] data) {
+		_g.initOverlays(data);
 	}
 	
 	class RequestTask extends AsyncTask<String, String, String>{
+		GeoInstagram _g;
+		
+		RequestTask(GeoInstagram g) {
+			_g = g;
+		}
+		
 	    @Override
 	    protected String doInBackground(String... uri) {
 	        HttpClient httpclient = new DefaultHttpClient();
@@ -54,7 +85,49 @@ public class GeoInstagram {
 	    @Override
 	    protected void onPostExecute(String result) {
 	        super.onPostExecute(result);
+	        
+	        ArrayList<InstaData> outstring = new ArrayList<InstaData>(100);
+	        
+	        try {
+				JSONObject response = new JSONObject(result);
+				JSONArray data = response.getJSONArray("data");
+				for (int i=0; i<data.length(); i++) {
+					JSONObject o = data.getJSONObject(i);
+					InstaData s = JsonParser.getImageLinkArray(o);
+					outstring.add(s);
+				}							
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	        
+	        InstaData [] arr = new InstaData[outstring.size()];
+	        for (int i=0; i<arr.length; i++) {
+	        	arr[i] = outstring.get(i);
+	        }
+	        
+	        
+	        Arrays.sort(arr, new Comparator<InstaData>() {
+	            public int compare(InstaData a, InstaData b) {
+	            	int c1 = Integer.valueOf(a.count);
+	            	int c2 = Integer.valueOf(b.count);
+	            	
+	            	if (c1 < c2) 
+	            		return 1;
+	            	else if (c1 > c2)
+	            		return -1;
+	            	else 
+	            		return 0;
+	            }
+	        });	
+	        
+	        for (InstaData a : outstring) {
+	        		Log.d(TAG, a.url);
+	        }
+	        
 	        Log.d(TAG, result);
+	        
+	        receivedPics(arr);
 	    }
 	}
 }
--- src/com/android/geogram/GeogramActivity.java
+++ src/com/android/geogram/GeogramActivity.java
@@ -1,44 +1,118 @@
 package com.android.geogram;
 
+import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.List;
+
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.location.LocationManager;
-import android.os.Bundle;
+
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.maps.GeoPoint;
+import com.google.android.maps.ItemizedOverlay;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapView;
 import com.google.android.maps.MyLocationOverlay;
+import com.google.android.maps.Overlay;
+import com.google.android.maps.OverlayItem;
+
 
 public class GeogramActivity extends MapActivity {
-	private static final int DEFAULT_ZOOM = 12;
+	private static final int DEFAULT_ZOOM = 13;
 	private static final String TAG = "Geogram";
 	
 	private LocationManager _locManager;
 	private GeoInstagram _geoInstagram;
+	private double _lat;
+	private double _long;
+	MapView mapView;
+    MyLocationOverlay mUserLocOverlay;
+	HelloItemizedOverlay _itemizedoverlay;
+	List<Overlay> _mapOverlays;
+	long unixTime, WeekFromNow, Recentl;
 	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main2);
+        setContentView(R.layout.main);
         
-        MapView mapView = (MapView) findViewById(R.id.mapview);
+        Button Recent = (Button) findViewById(R.id.recent);
+        Button Best = (Button) findViewById(R.id.best);
+        
+        //setActionBarContentView(R.layout.main);
+        
+        
+        
+        mapView = (MapView) findViewById(R.id.mapview);
         mapView.setBuiltInZoomControls(true);
         mapView.getController().setZoom(DEFAULT_ZOOM);
         
         _locManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-        centerOnLocation(mapView, _locManager);       
+        centerOnLocation(mapView, _locManager);    
         
-        MyLocationOverlay mUserLocOverlay = new MyLocationOverlay(this, mapView);
-		mapView.getOverlays().add(mUserLocOverlay);
-		
+        Drawable drawable = this.getResources().getDrawable(R.drawable.ga_map_pin);
+        _itemizedoverlay = new HelloItemizedOverlay(drawable, this);
+        
+        mUserLocOverlay = new MyLocationOverlay(this, mapView);
+        mapView.getOverlays().add(mUserLocOverlay);		
 		mUserLocOverlay.enableMyLocation();
 		mUserLocOverlay.enableCompass();
 
-		_geoInstagram = new GeoInstagram();
-		_geoInstagram.getPics();
+		_geoInstagram = new GeoInstagram(this);
+		
+		unixTime = System.currentTimeMillis() / 1000L;
+		WeekFromNow = unixTime - 604800000 / 1000L;
+		Recentl = unixTime - 86400000 / 1000L;
+		
+		
+				
+		_geoInstagram.getPics(_lat, _long, WeekFromNow);
+		
+		
+		Recent.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View v) {
+				// Switching to Register screen
+				mapView.getOverlays().clear();
+				mapView.invalidate();
+				mapView.getOverlays().add(mUserLocOverlay);		
+				mUserLocOverlay.enableMyLocation();
+				mUserLocOverlay.enableCompass();
+
+				_geoInstagram.getPics(_lat, _long);
+				
+				
+			}
+		});
+		
+		Best.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View v) {
+				// Switching to Register screen
+				mapView.getOverlays().clear();
+				mapView.invalidate();
+				mapView.getOverlays().add(mUserLocOverlay);		
+				mUserLocOverlay.enableMyLocation();
+				mUserLocOverlay.enableCompass();
+				_geoInstagram.getPics(_lat, _long, WeekFromNow);
+				
+				
+			}
+		});
+		
+		/*
+    	GeoPoint point = new GeoPoint((int)(1E6*_lat), (int)(1E6*_long));
+        OverlayItem item = new OverlayItem(point, "", "");
+        _itemizedoverlay.addOverlay(item);	
+        mapView.getOverlays().add(_itemizedoverlay);
+		*/
     }
      
     public void centerOnLocation(MapView mapView, LocationManager locManager) {
@@ -56,15 +130,42 @@ public class GeogramActivity extends MapActivity {
 		}
 	}
    
-    public static GeoPoint locationToGeoPoint(Location mLocation) {
-		double mLat = mLocation.getLatitude();
-		double mLon = mLocation.getLongitude();
-		GeoPoint newPoint = new GeoPoint((int) (mLat * 1e6), (int) (mLon * 1e6));
+    public GeoPoint locationToGeoPoint(Location mLocation) {
+		_lat = mLocation.getLatitude();
+		_long = mLocation.getLongitude();
+		GeoPoint newPoint = new GeoPoint((int) (_lat * 1e6), (int) (_long * 1e6));
 		return newPoint;
 	}
     
     @Override
     protected boolean isRouteDisplayed() {
         return false;
+    }
+    
+    public void initOverlays(InstaData[] data){
+    	int num = 0;
+    	
+    	Log.i(TAG, "initOverlays() called");
+    	for (InstaData s : data) {
+    		String uri = s.url;
+    		double lat = Double.valueOf(s.latitude);
+    		double lon = Double.valueOf(s.longitude);
+    		String id = String.valueOf(s.id);
+    		
+    		Log.i(TAG, "URI = " + uri + "lat " + lat);
+    		
+        	GeoPoint point = new GeoPoint((int)(1E6*lat), (int)(1E6*lon));
+            OverlayItem item = new OverlayItem(point, id+":" + String.valueOf(num+1), uri);
+            _itemizedoverlay.addOverlay(item);
+            
+            
+            num++;
+            if (num == 7) {
+            	break;
+            }
+            
+    	}
+    	
+    	mapView.getOverlays().add(_itemizedoverlay);
     }
 }
